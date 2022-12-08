bc11d5a22714a9d63a15cd3cc7cb9e788d433135
fortune48 <ukwuomafortune01@gmail.com>
2022-12-08 08:50:10 +0100

    restrict views at controller level

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
-
   end
 
   def create
@@ -20,5 +20,13 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "Logged out."
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+    end
   end
 end
